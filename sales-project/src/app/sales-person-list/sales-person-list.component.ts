b6bca302d51ffa512492d3647da47b2d7e655ef4
Victor Olamide <akinnusivictor098@gmail.com>
2022-12-07 21:54:05 +0000

    Integrating Angular with Bootstrap :-)

--- sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -3,16 +3,16 @@ import {SalesPerson} from "./sales-person";
 
 @Component({
   selector: 'app-sales-person-list',
-  templateUrl: './sales-person-list.component.html',
+  templateUrl: './sales-person-list-bootstrap.component.html',
   styleUrls: ['./sales-person-list.component.scss']
 })
 export class SalesPersonListComponent implements OnInit {
 
   salesPersonList: SalesPerson[] = [
-    new SalesPerson("Austin","Victor","olamide@gmail.com",6000),
-    new SalesPerson("Susan","Opeyemi","opeyemi@gmail.com",5000),
-    new SalesPerson("King", "Lebron","lebron@gmail.com",2000),
-    new SalesPerson("Steph","Curry","curry@gmail.com",3000)
+    new SalesPerson("Austin","Victor","olamide@gmail.com",60000),
+    new SalesPerson("Susan","Opeyemi","opeyemi@gmail.com",50000),
+    new SalesPerson("King", "Lebron","lebron@gmail.com",20000),
+    new SalesPerson("Steph","Curry","curry@gmail.com",70000)
   ];
   constructor() { }
 
